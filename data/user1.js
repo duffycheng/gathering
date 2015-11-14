f690760c899c5573584a94f735e008c3a9e3dc96
dominic <dominic@thetigerparty.com>
2015-11-15 00:43:42 +0800

    Implement map popup window

---
+++ data/user1.js
@@ -0,0 +1,411 @@
+window.GATHERING_DATA.user1 = {
+  "data": [
+    {
+      "id": "10204302541197837",
+      "created_time": "2015-11-14T13:11:11+0000",
+      "place": {
+        "id": "676999962415610",
+        "location": {
+          "city": "Taipei",
+          "country": "Taiwan",
+          "latitude": 25.0450592,
+          "longitude": 121.5319138,
+          "street": "中正區市民大道三段二號",
+          "zip": "100"
+        },
+        "name": "SYNTREND 三創生活園區"
+      }
+    },
+    {
+      "id": "10204117208284630",
+      "created_time": "2015-09-25T04:44:56+0000",
+      "place": {
+        "id": "278810045615410",
+        "location": {
+          "city": "Taipei",
+          "country": "Taiwan",
+          "latitude": 25.0280895,
+          "longitude": 121.5479202,
+          "street": "大安區四維路170巷2號",
+          "zip": "106"
+        },
+        "name": "小巴黎人 Bookshelf Bakery"
+      }
+    },
+    {
+      "id": "10204017551873282",
+      "created_time": "2015-09-05T11:36:42+0000",
+      "place": {
+        "id": "802040419806588",
+        "location": {
+          "city": "Taichung",
+          "country": "Taiwan",
+          "latitude": 24.1590004,
+          "longitude": 120.6457672,
+          "street": "台中市西屯區府會園道6號",
+          "zip": "407"
+        },
+        "name": "叉子"
+      }
+    },
+    {
+      "id": "10203935854510899",
+      "created_time": "2015-08-22T06:47:38+0000",
+      "place": {
+        "id": "189969011072707",
+        "location": {
+          "city": "Taichung",
+          "country": "Taiwan",
+          "latitude": 24.187858092864,
+          "longitude": 120.59367173193
+        },
+        "name": "春水堂（東海藝術街店）"
+      }
+    },
+    {
+      "id": "10203796459906121",
+      "created_time": "2015-07-25T07:37:24+0000",
+      "place": {
+        "id": "672612389492890",
+        "location": {
+          "city": "Taichung",
+          "country": "Taiwan",
+          "latitude": 24.180426896476,
+          "longitude": 120.62183583355,
+          "street": "台中市西屯區福安路65號(近台灣大道四段)",
+          "zip": "407"
+        },
+        "name": "藝人豬腳 · 薏仁豬腳"
+      }
+    },
+    {
+      "id": "10203741781779202",
+      "created_time": "2015-07-14T13:03:22+0000",
+      "place": {
+        "id": "138099936255786",
+        "location": {
+          "city": "Taipei",
+          "country": "Taiwan",
+          "latitude": 25.035287723211,
+          "longitude": 121.56586146226,
+          "street": "松壽路12號",
+          "zip": "110-51"
+        },
+        "name": "ATT 4 FUN"
+      }
+    },
+    {
+      "id": "10203439332218152",
+      "created_time": "2015-05-19T14:25:12+0000",
+      "place": {
+        "id": "187239064633132",
+        "location": {
+          "city": "Taipei",
+          "country": "Taiwan",
+          "latitude": 25.035859257215,
+          "longitude": 121.56920776787,
+          "street": "信義區松仁路101號",
+          "zip": "110"
+        },
+        "name": "Brown Sugar Live & Restaurant 黑糖餐廳"
+      }
+    },
+    {
+      "id": "10203385003459967",
+      "created_time": "2015-05-08T11:12:32+0000",
+      "place": {
+        "id": "164806986904076",
+        "location": {
+          "city": "Taipei",
+          "country": "Taiwan",
+          "latitude": 25.0423908,
+          "longitude": 121.5649567,
+          "street": "基隆路一段178號6F",
+          "zip": "110"
+        },
+        "name": "AppWorks Ventures 之初創投"
+      }
+    },
+    {
+      "id": "10203367534303249",
+      "created_time": "2015-05-03T06:41:53+0000",
+      "place": {
+        "id": "245191565657534",
+        "location": {
+          "city": "Tainan",
+          "country": "Taiwan",
+          "latitude": 22.939245781658,
+          "longitude": 120.2310224164,
+          "street": "臺南市 仁德區 文華路二段326號",
+          "zip": "717"
+        },
+        "name": "十鼓仁糖文創園區"
+      }
+    },
+    {
+      "id": "10203358789124625",
+      "created_time": "2015-05-01T04:49:42+0000",
+      "place": {
+        "id": "295886863938727",
+        "location": {
+          "city": "Taipei",
+          "country": "Taiwan",
+          "latitude": 25.0388508,
+          "longitude": 121.5670319,
+          "street": "3F, No. 16, Songgao Road, Xinyi District",
+          "zip": "110"
+        },
+        "name": "Texas Roadhouse Taiwan 德州鮮切牛排"
+      }
+    },
+    {
+      "id": "10203359796869818",
+      "created_time": "2015-05-01T03:54:58+0000",
+      "place": {
+        "id": "1558187104394819",
+        "location": {
+          "city": "Taipei",
+          "country": "Taiwan",
+          "latitude": 25.037892083112,
+          "longitude": 121.56708535681,
+          "street": "信義區松高路16號",
+          "zip": "110"
+        },
+        "name": "H&M 微風松高店"
+      }
+    },
+    {
+      "id": "10203327229695659",
+      "created_time": "2015-04-25T03:20:36+0000",
+      "place": {
+        "id": "194148113950168",
+        "location": {
+          "city": "Taichung",
+          "country": "Taiwan",
+          "latitude": 24.2265491,
+          "longitude": 120.5755005,
+          "street": "台中市沙鹿區北勢東路526-1號",
+          "zip": "433"
+        },
+        "name": "布袋海之蚵"
+      }
+    },
+    {
+      "id": "10203323210155173",
+      "created_time": "2015-04-24T11:55:15+0000",
+      "place": {
+        "id": "548409401893521",
+        "location": {
+          "city": "Taipei",
+          "country": "Taiwan",
+          "latitude": 25.0205002,
+          "longitude": 121.5312271,
+          "street": "台北市大安區羅斯福路三段269巷58號",
+          "zip": "106"
+        },
+        "name": "Venture Lab. 創業酒吧"
+      }
+    },
+    {
+      "id": "10203301466451594",
+      "created_time": "2015-04-20T04:50:22+0000",
+      "place": {
+        "id": "744742458930005",
+        "location": {
+          "city": "Taipei",
+          "country": "Taiwan",
+          "latitude": 25.054033120769,
+          "longitude": 121.59225188065,
+          "street": "台北市南港區昆陽街2號2樓",
+          "zip": "115"
+        },
+        "name": "心食活力吧"
+      }
+    },
+    {
+      "id": "10203266230010705",
+      "created_time": "2015-04-13T16:45:34+0000",
+      "place": {
+        "id": "794140310648740",
+        "location": {
+          "city": "Hsinchuang",
+          "country": "Taiwan",
+          "latitude": 25.063049453824,
+          "longitude": 121.500446455,
+          "street": "(24148) 新北市三重區重新路一段108號3樓(捷運台北橋站3樓)"
+        },
+        "name": "Innosquare 新北創力坊"
+      }
+    },
+    {
+      "id": "10203255526983136",
+      "created_time": "2015-04-11T15:16:12+0000",
+      "place": {
+        "id": "802040419806588",
+        "location": {
+          "city": "Taichung",
+          "country": "Taiwan",
+          "latitude": 24.1590004,
+          "longitude": 120.6457672,
+          "street": "台中市西屯區府會園道6號",
+          "zip": "407"
+        },
+        "name": "叉子"
+      }
+    },
+    {
+      "id": "10203213022360547",
+      "created_time": "2015-04-03T10:51:00+0000",
+      "place": {
+        "id": "217690888390415",
+        "location": {
+          "city": "Taichung",
+          "country": "Taiwan",
+          "latitude": 24.1875,
+          "longitude": 120.5945969,
+          "street": "434 台中市龍井區藝術街23號1樓",
+          "zip": "434"
+        },
+        "name": "春天．漫慢"
+      }
+    },
+    {
+      "id": "10203104657691498",
+      "created_time": "2015-03-15T11:56:13+0000",
+      "place": {
+        "id": "659928564025124",
+        "location": {
+          "city": "Taichung",
+          "country": "Taiwan",
+          "latitude": 24.187701442786,
+          "longitude": 120.5933428167,
+          "street": "台中市龍井區藝術街61巷3號之5",
+          "zip": "43446"
+        },
+        "name": "小巷子豆花"
+      }
+    },
+    {
+      "id": "10203100118778028",
+      "created_time": "2015-03-14T14:58:47+0000",
+      "place": {
+        "id": "399094955116",
+        "location": {
+          "city": "Taichung",
+          "country": "Taiwan",
+          "latitude": 24.1844196,
+          "longitude": 120.6211472,
+          "street": "台中市西屯區永福路118巷3號"
+        },
+        "name": "Easy Pub Taichung"
+      }
+    },
+    {
+      "id": "10203092752513876",
+      "created_time": "2015-03-13T12:48:07+0000",
+      "place": {
+        "id": "327595180672091",
+        "location": {
+          "city": "Taipei",
+          "country": "Taiwan",
+          "latitude": 25.0667191,
+          "longitude": 121.5368881,
+          "street": "台北市中山區民族東路410巷2弄18號",
+          "zip": "10473"
+        },
+        "name": "三井上引水產"
+      }
+    },
+    {
+      "id": "10203065977364514",
+      "created_time": "2015-03-08T20:12:02+0000",
+      "place": {
+        "id": "144142625619197",
+        "location": {
+          "city": "San Francisco",
+          "country": "United States",
+          "latitude": 37.795839769422,
+          "longitude": -122.39386402425,
+          "state": "CA",
+          "street": "1 Ferry Building",
+          "zip": "94111"
+        },
+        "name": "Blue Bottle Coffee"
+      }
+    },
+    {
+      "id": "10202979491322417",
+      "created_time": "2015-02-19T23:59:59+0000",
+      "place": {
+        "id": "6192688417",
+        "location": {
+          "city": "Stanford",
+          "country": "United States",
+          "latitude": 37.429379300231,
+          "longitude": -122.16737570553,
+          "state": "CA",
+          "street": "450 Serra Mall",
+          "zip": "94305"
+        },
+        "name": "Stanford University"
+      }
+    },
+    {
+      "id": "10202974448156341",
+      "created_time": "2015-02-18T22:06:30+0000",
+      "place": {
+        "id": "14135965383",
+        "location": {
+          "city": "San Francisco",
+          "country": "United States",
+          "latitude": 37.787415341856,
+          "longitude": -122.41378844505,
+          "state": "CA",
+          "street": "711 Post St",
+          "zip": "94109"
+        },
+        "name": "USA Hostels San Francisco"
+      }
+    },
+    {
+      "id": "10202973072361947",
+      "created_time": "2015-02-18T15:48:16+0000",
+      "place": {
+        "id": "113529652035965",
+        "location": {
+          "city": "San Francisco",
+          "country": "United States",
+          "latitude": 37.7871933,
+          "longitude": -122.4106216,
+          "state": "CA",
+          "street": "442 Geary St",
+          "zip": "941021223"
+        },
+        "name": "Starbucks"
+      }
+    },
+    {
+      "id": "10202971135433525",
+      "created_time": "2015-02-18T04:44:28+0000",
+      "place": {
+        "id": "527299747282567",
+        "location": {
+          "city": "Los Angeles",
+          "country": "United States",
+          "latitude": 34.05515,
+          "longitude": -118.23525,
+          "state": "CA",
+          "zip": "90012"
+        },
+        "name": "Union Station (Los Angeles)"
+      }
+    }
+  ],
+  "paging": {
+    "cursors": {
+      "before": "MTAyMDQzMDI1NDExOTc4MzcZD",
+      "after": "MTAyMDI5NzExMzU0MzM1MjUZD"
+    },
+    "next": "https://graph.facebook.com/v2.5/10201302975570571/tagged_places?access_token=CAACEdEose0cBAN5GLctObbHFbV5CLKBcmDhtRwgcMX3ZBZBbkd2p3oYxX8DcKxzswbZBwGRlABpzTsbMVXfeRZBFHFG9phk4zhaptKZBIUjsCJKMnSm2aV4vTchD3m0egDucqw5iMFQpGNVLRb2sM5qltqRhnDprIBRsvr7xRrZBJYSZBw9Fi9X015KfEIrRkyySW2HynZCHIkxHYpFr4ZAFp&pretty=0&limit=25&after=MTAyMDI5NzExMzU0MzM1MjUZD"
+  }
+}
--- js/main.js
+++ js/main.js
@@ -6,8 +6,54 @@ GatheringApp.config(function($interpolateProvider, $logProvider) {
     $logProvider.debugEnabled(false);
 });
 
-GatheringApp.controller('MapCtrl', function() {
+GatheringApp.service('MapService', function() {
+	that = this;
+
+	this.map = {
+		center: {
+            lat: 25.0453633,
+            lng: 121.528795,
+            zoom: 12
+        },
+        defaults: {
+	        tileLayer: "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
+	        zoomControlPosition: 'topright',
+	        tileLayerOptions: {
+	            opacity: 0.9,
+	            detectRetina: true,
+	            reuseTiles: true,
+	        },
+	        scrollWheelZoom: false
+	    },
+	    markers: {
+
+	    },
+        events: {
+        }
+	};
+
+	this.initMap = function () {
+		return that.map;
+	};
+
+	this.addUsersIntoMarkers = function(user_tag_list) {
+
+	};
+
+	this.addMarkers = function (data_list) {
+		angular.forEach(data_list, function(data, index) {
+			that.map.markers[index] = {
+				lat : parseFloat(data.lat),
+				lng : parseFloat(data.lng),
+				message : '<popup index="'+ index +'"></popup>'
+			};
+		});
+	};
+});
+
+GatheringApp.controller('MapCtrl', function (MapService) {
 	mcScope = this;
+
 	this.categories = [
 		{
 			id: 1,
@@ -26,27 +72,23 @@ GatheringApp.controller('MapCtrl', function() {
 			name: 'gym'
 		}
 	];
+	(function init() {
+		mcScope.map = MapService.initMap();
+		mcScope.users = window.GATHERING_DATA.user1;
+		MapService.addMarkers(window.GATHERING_DATA.gym)
+	})();
+});
 
-	this.map = {
-		center: {
-            lat: 52.374004,
-            lng: 4.890359,
-            zoom: 7
-        },
-        defaults: {
-	        tileLayer: "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
-	        zoomControlPosition: 'topright',
-	        tileLayerOptions: {
-	            opacity: 0.9,
-	            detectRetina: true,
-	            reuseTiles: true,
-	        },
-	        scrollWheelZoom: false
-	    },
-	    markers: {
-
-	    },
-        events: {
-        }
+GatheringApp.directive('popup', function() {
+	return {
+		restrict: "E",
+		replace: true,
+		scope: {
+			index: '='
+		},
+		link: function(scope, element, attrs) {
+			console.log(scope.index);
+		},
+		templateUrl: '/partials/popup.html'
 	}
 });
